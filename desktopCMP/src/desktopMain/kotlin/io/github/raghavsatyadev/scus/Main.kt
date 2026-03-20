f4e870207e4255027d56830cefef08deb9cf398e
Dev4Life <vishrut.goyani9@gmail.com>
2026-03-20 17:14:05 +0530

    - Added iOS target

--- desktopCMP/src/desktopMain/kotlin/io/github/raghavsatyadev/scus/Main.kt
+++ desktopCMP/src/desktopMain/kotlin/io/github/raghavsatyadev/scus/Main.kt
@@ -2,8 +2,18 @@ package io.github.raghavsatyadev.scus
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import io.github.raghavsatyadev.library.support.initKoin
 import io.github.raghavsatyadev.library.support.theme.AppTheme
+import io.github.raghavsatyadev.library.ui.main.MainScreen
 
 fun main() {
-  application { Window(onCloseRequest = ::exitApplication, title = "KMP App") { AppTheme {} } }
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "KMP App"
+        ) {
+            initKoin()
+            AppTheme { MainScreen { } }
+        }
+    }
 }
